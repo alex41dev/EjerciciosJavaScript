90bcf04084a181d0558d0d3064723bd5ea407484
alex41dev <113487154+alex41dev@users.noreply.github.com>
2022-11-08 22:15:15 -0500

    Primer ejercicio de JS

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// comentario de linea -> control + }
+
+/* Comentario de bloque -> alt + shift + a */
+
+alert("Hola mundo, estoy aprendiendo javascript");
+alert("dos");
+alert("tres");
+
+console.log("Hola este es un mensaje por consola");
+console.log(123456);
+console.log(true);
+
+document.write("Imprimendo en página web");
